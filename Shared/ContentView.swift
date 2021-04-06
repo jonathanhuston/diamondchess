f64a1a6df7ca6a7f53883b4e46e5f9e259a35a1b
Jonathan Huston <jonathan.huston@gmail.com>
2021-04-06 14:53:27 +0200

    initial board

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,43 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView {
+    let board = [["Black Rook", "Black Knight", "Black Bishop", "Black Queen", "Black King", "Black Bishop", "Black Knight", "Black Rook"],
+                 ["Black Pawn", "Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn"],
+                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                 ["White Pawn", "White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn"],
+                 ["White Rook", "White Knight", "White Bishop", "White Queen", "White King", "White Bishop", "White Knight", "White Rook"]]
+}
+
+extension ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(Int(0)...Int(7), id:\.self) { rank in
+                HStack {
+                    ForEach(Int(0)...Int(7), id:\.self) { file in
+                        ZStack {
+                            if (rank % 2 == 0 && file % 2 == 0) || (rank % 2 == 1 && file % 2 == 1) {
+                                Image("White Empty")
+                                    .resizable()
+                                    .frame(width: 120)
+                            } else {
+                                Image("Black Empty")
+                                    .resizable()
+                                    .frame(width: 120)
+                            }
+                            if board[rank][file] != "Empty" {
+                                Image(board[rank][file])
+                                    .resizable()
+                                    .frame(width: 80, height: 100)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
