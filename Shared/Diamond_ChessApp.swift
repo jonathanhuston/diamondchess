3ed212fd2c0502220378520c3b1c583cb62d3fe9
Jonathan Huston <jonathan.huston@gmail.com>
2021-04-06 13:40:04 +0200

    Initial Commit

---
+++ Shared/Diamond_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diamond_ChessApp.swift
+//  Shared
+//
+//  Created by Jonathan Huston on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Diamond_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
