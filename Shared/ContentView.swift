957d23d82bd76af5147ab9fc7bcbda5f1100e94e
Jonathan Huston <jonathan.huston@gmail.com>
2021-04-07 10:03:32 +0200

    refactor, resize

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,6 @@
 import SwiftUI
 
 struct ContentView {
-    let board = [["Black Rook", "Black Knight", "Black Bishop", "Black Queen", "Black King", "Black Bishop", "Black Knight", "Black Rook"],
-                 ["Black Pawn", "Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn"],
-                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
-                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
-                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
-                 ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
-                 ["White Pawn", "White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn"],
-                 ["White Rook", "White Knight", "White Bishop", "White Queen", "White King", "White Bishop", "White Knight", "White Rook"]]
 }
 
 extension ContentView: View {
@@ -28,16 +20,16 @@ extension ContentView: View {
                             if (rank % 2 == 0 && file % 2 == 0) || (rank % 2 == 1 && file % 2 == 1) {
                                 Image("White Empty")
                                     .resizable()
-                                    .frame(width: 120)
+                                    .frame(width: 110, height: 110)
                             } else {
                                 Image("Black Empty")
                                     .resizable()
-                                    .frame(width: 120)
+                                    .frame(width: 110, height: 110)
                             }
-                            if board[rank][file] != "Empty" {
-                                Image(board[rank][file])
+                            if newBoard[rank][file] != "Empty" {
+                                Image(newBoard[rank][file])
                                     .resizable()
-                                    .frame(width: 80, height: 100)
+                                    .frame(width: 80, height: 105)
                             }
                         }
                     }
---
+++ Shared/GlobalDefinitions.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalDefinitions.swift
+//  Diamond Chess
+//
+//  Created by Jonathan Huston on 4/7/21.
+//
+
+import Foundation
+
+let newBoard = [["Black Rook", "Black Knight", "Black Bishop", "Black Queen", "Black King", "Black Bishop", "Black Knight", "Black Rook"],
+                ["Black Pawn", "Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn","Black Pawn"],
+                ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                ["Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"],
+                ["White Pawn", "White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn","White Pawn"],
+                ["White Rook", "White Knight", "White Bishop", "White Queen", "White King", "White Bishop", "White Knight", "White Rook"]]
