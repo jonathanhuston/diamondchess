6d2381caa3d1268ba3a6c1d4eb4b5d7b8af436f4
Jonathan Huston <jonathan.huston@gmail.com>
2021-04-09 13:37:34 +0200

    refactor

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,38 +8,12 @@
 import SwiftUI
 
 struct ContentView {
+    @EnvironmentObject var game: Game
 }
 
 extension ContentView: View {
     var body: some View {
-        VStack {
-            ForEach(Int(0)...Int(7), id:\.self) { rank in
-                HStack {
-                    ForEach(Int(0)...Int(7), id:\.self) { file in
-                        ZStack {
-                            if (rank % 2 == 0 && file % 2 == 0) || (rank % 2 == 1 && file % 2 == 1) {
-                                Image("White Empty")
-                                    .resizable()
-                                    .frame(width: 110, height: 110)
-                            } else {
-                                Image("Black Empty")
-                                    .resizable()
-                                    .frame(width: 110, height: 110)
-                            }
-                            if newBoard[rank][file].contains("Pawn") {
-                                Image(newBoard[rank][file])
-                                    .resizable()
-                                    .frame(width: 50, height: 80)
-                            } else if newBoard[rank][file] != "Empty" {
-                                Image(newBoard[rank][file])
-                                    .resizable()
-                                    .frame(width: 80, height: 105)
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        BoardView()
     }
 }
 
