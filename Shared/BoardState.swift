2f52da919604eda26dc495354e066e85e749ae1b
Jonathan Huston <jonathan.huston@gmail.com>
2021-04-11 11:49:25 +0200

    en passant

--- Shared/BoardState.swift
+++ Shared/BoardState.swift
@@ -8,8 +8,9 @@
 struct BoardState {
     var board: Board = newBoard
     var currentPlayer: Player = .white
+    var enPassantSquare: Square? = nil
     
-    // TODO: en passant, promotion
+    // TODO: promotion
     private func pawnMoves(from: Square) -> [Square] {
         var moves = [Square]()
         let player = color(of: self.board[from.rank][from.file])
@@ -22,8 +23,14 @@ struct BoardState {
         }
         
         for toFile in [from.file - 1, from.file + 1] {
-            if 0...7 ~= toFile && color(of: self.board[toRank][toFile]) == opponent[player] {
-                moves.append(Square(rank: toRank, file: toFile))
+            if 0...7 ~= toFile {
+                if color(of: self.board[toRank][toFile]) == opponent[player] {
+                    moves.append(Square(rank: toRank, file: toFile))
+                } else if let enPassant = self.enPassantSquare {
+                    if enPassant == Square(rank: toRank, file: toFile) {
+                        moves.append(enPassant)
+                    }
+                }
             }
         }
         
@@ -50,6 +57,37 @@ struct BoardState {
         return false
     }
     
+    private func enPassantSquare(for piece: String, from: Square, to: Square) -> Square? {
+        if !piece.contains("Pawn") {
+            return nil
+        }
+        
+        if abs(from.rank - to.rank) != 2 {
+            return nil
+        }
+        
+        return Square(rank: (from.rank + to.rank) / 2, file: from.file)
+    }
+    
+    private func removeEnPassantPawn(for piece: String, from: Square, to: Square) -> BoardState {
+        if !piece.contains("Pawn") {
+            return self
+        }
+        
+        if from.file == to.file {
+            return self
+        }
+        
+        if enPassantSquare == nil || enPassantSquare != to {
+            return self
+        }
+        
+        var newBoardState = self
+        newBoardState.board[from.rank][to.file] = "Empty"
+        
+        return  newBoardState
+    }
+    
     func isValidMove(for piece: String, from: Square, to: Square) -> BoardState? {
         if !(0...7 ~= to.rank && 0...7 ~= to.file) {
             return nil
@@ -63,15 +101,16 @@ struct BoardState {
             return nil
         }
                 
-        var newBoardState = self
+        var newBoardState = self.removeEnPassantPawn(for: piece, from: from, to: to)
         
         newBoardState.board[from.rank][from.file] = "Empty"
         newBoardState.board[to.rank][to.file] = piece
-        
+
         if newBoardState.inCheck(player: color(of: piece)) {
             return nil
         }
         
+        newBoardState.enPassantSquare = enPassantSquare(for: piece, from: from, to: to)
         newBoardState.currentPlayer = opponent[self.currentPlayer]!
                 
         return newBoardState
