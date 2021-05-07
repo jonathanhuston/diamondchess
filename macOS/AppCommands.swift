9c73f5e344b67a9fa04461dcdde556916dbfc10d
Jonathan Huston <jonathan.huston@gmail.com>
2021-05-07 22:36:05 +0200

    help menu

--- macOS/AppCommands.swift
+++ macOS/AppCommands.swift
@@ -7,8 +7,21 @@
 
 import SwiftUI
 
-// FIX: Help menu
-struct AppCommands: Commands {
+struct AppCommands {
+    @Environment(\.openURL) var openURL
+        
+    private let baseURL = "http://diamondchess.club/"
+    
+    private var legalDocs = ["Privacy"]
+    
+    private let nsView = NSView()
+    
+    func webpage(_ menuItem: String) -> String {
+        baseURL + menuItem.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)! + ".html"
+    }
+}
+
+extension AppCommands: Commands {
     
     @CommandsBuilder var body: some Commands {
         CommandGroup(replacing: .appInfo) {
@@ -24,6 +37,24 @@ struct AppCommands: Commands {
                         NSApplication.AboutPanelOptionKey(rawValue: "Copyright"): "© 2021 Jonathan Huston"
                     ]
                 )
+            }
+        }
+        
+        CommandGroup(replacing: .help) {
+            Button("Diamond Chess Help") {
+                if let url = URL(string: baseURL) {
+                    openURL(url)
+                }
+            }
+            
+            Divider()
+            
+            ForEach(legalDocs, id:\.self) { doc in
+                Button(doc) {
+                    if let url = URL(string: webpage(doc)) {
+                        openURL(url)
+                    }
+                }
             }
         }
     }
