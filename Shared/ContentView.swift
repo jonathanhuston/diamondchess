4b8eff5af61cec6f4bdcddb99ae1ad6fd13df95e
Jonathan Huston <jonathan.huston@gmail.com>
2021-06-06 16:08:05 +0200

    moves in navigation bar

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,12 +11,32 @@ struct ContentView {
     @EnvironmentObject var game: Game
     
     private func navigationText() -> String {
+        let player = game.boardState.currentPlayer
+        
         var text = ""
         
-        text += "\(game.boardState.currentPlayer.rawValue)'s turn"
+        switch game.boardState.winner {
+        case .white:
+            text += "1–O  "
+        case .black:
+            text += "0–1  "
+        case .empty:
+            text += "½–½  "
+        default:
+            text += "\(player.rawValue)'s turn  "
+        }
         
         if game.computerPlayer != nil {
-            text += "  –  \(strengths[game.strength]!)\t\t"
+            text += "–  \(strengths[game.strength]!) "
+        }
+        
+        if game.moves.count > 0 {
+            let moveCount = (game.moves.count + 1) / 2
+            let moveIndicator = player == .white ? ". ... " : ". "
+            let mateIndicator = game.boardState.winner == opponent[player]! ? "#" : ""
+            let checkIndicator = game.boardState.inCheck[player]! && game.boardState.winner == nil ? "+" : ""
+            
+            text += "(\(moveCount)\(moveIndicator)\(game.moves.last!)\(mateIndicator)\(checkIndicator))"
         }
         
         return text
